f41dbe1ceae47febaecc9a85b6e4b378f55b611c
Perry <petreli1@ymail.com>
2022-05-10 12:54:43 +0100

    planning code structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import SignIn from './componenets/routes/SignIn';
+import Home from './componenets/routes/Home';
+import Profile from './componenets/routes/Profile';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(props) {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<SignIn />} />
+                <Route path="/home" element={<Home />} />
+                <Route path="/:id" element={<Profile />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
