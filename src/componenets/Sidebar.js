90fe9076ed8117fa532a2b24cd2ceb3d901b86e3
Perry <petreli1@ymail.com>
2022-05-16 16:27:14 +0100

    added 'who to follow' section styling

--- src/componenets/Sidebar.js
+++ src/componenets/Sidebar.js
@@ -2,46 +2,54 @@ import React from 'react';
 import style from './styles/sidebar.module.css';
 import Search from './Search.js';
 
+//temp img
+import image from '../images/profile.png';
+
 function Sidebar(props) {
     const {explore} = props
 
-    if (!explore) {
-        return (
-            <div className={style.mainContainer}>
+    return (
+        <div className={style.mainContainer}>
+            {!explore && 
                 <Search />
-                <div className={style.emptyContainer}/>
-                <div>
-                    <WhatsHappening />
-                    <Follow />
-                    <Details />
-                </div>
-            </div>
-        )
-    } else {
-        return (
+            }
+            <div className={style.emptyContainer}/>
             <div>
                 <Follow />
                 <Details />
             </div>
-        );
-    }
-    
-}
-
-const WhatsHappening = (props) => {
-    return (
-        <div>
-            whats happening
         </div>
     );
 }
 
 const Follow = (props) => {
     return (
-        <div>
-            follow
+        <div className={style.follow}>
+            <h2 className={style.followTitle}>Who to follow</h2>
+            {/*temp values for styling*/}
+            <Suggestion src={image} name="Michael Reeves" handle="michaelreeves"/>
+            <Suggestion src={image} name="Why I Hate this Album" handle="AlbumHatePod"/>
+            <Suggestion src={image} name="Killer Queen" handle="Kira"/>
+            <p className={style.showMore}>Show More</p>
         </div>
     );
+}
+
+const Suggestion = (props) => {
+    const {src, name, handle} = props;
+
+    return (
+        <div className={style.suggestion}>
+            <div className={style.suggestionImg}>
+                <img alt='' src={src}/>
+            </div>
+            <div className={style.suggestionNames}>
+                <p className={style.displayName}>{name}</p>
+                <p className={style.handle}>{handle}</p>
+            </div>
+            <button className={style.followButton}>Follow</button>
+        </div>
+    )
 }
 
 const Details = (props) => {
