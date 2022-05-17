c2a327dcf8d6fafd6df37f790b1b6d3697c66c8a
Perry <petreli1@ymail.com>
2022-05-17 16:02:43 +0100

    styling for how post are displayed

--- src/componenets/Search.js
+++ src/componenets/Search.js
@@ -6,13 +6,14 @@ import searchIcon from '../images/explore.png'
 function Search(props) {
     return (
         <div className={style.container}>
-            <div className={style.position}>
-                <div className={style.searchImg}>
-                    <img src={searchIcon} alt='' />
+            <div className={style.subContainer}>
+                <div className={style.position}>
+                    <div className={style.searchImg}>
+                        <img src={searchIcon} alt='' />
+                    </div>
+                    <input placeholder='Search Twitter' className={style.searchInput}/>   
                 </div>
-                <input placeholder='Search Twitter' className={style.searchInput}/>   
             </div>
-            
         </div>
     );
 }
--- src/componenets/routes/Home.js
+++ src/componenets/routes/Home.js
@@ -2,9 +2,21 @@ import React, { Component } from 'react';
 import Sidebar from '../Sidebar.js';
 import topStyle from '../styles/topbar.module.css';
 import style from '../styles/home.module.css';
+import uniqid from 'uniqid';
 
-//temp image
+//temp image for styling
 import pic from '../../images/logo.png';
+
+//temp tweet info for styling
+const fakeTweet = [
+    {user:{name:'PlaceHolder', handle:'placeholder', pic:pic}, time:new Date(), content:{text:'placeholder text on my placeholder tweet I am making a placeholder, lots of text lots of tweet not enough placeholder wwwwwwwwwwwwwwwwwwwwwwww wwwwwwwwwwwwwww wwwwwwwww wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww wwwwwwwwwwwwwwwwwwwww wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww', img:pic, ref:false}, key:uniqid()},
+    {user:{name:'PlaceHolder', handle:'placeholder', pic:pic}, time:new Date(), content:{text:'placeholder text on my placeholder tweet I am making a placeholder, lots of text lots of tweet not enough placeholder wwwwwwwwwwwwwwwwwwwwwwww wwwwwwwwwwwwwww wwwwwwwww wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww wwwwwwwwwwwwwwwwwwwww wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww', img:undefined, ref:false}, key:uniqid()},
+    {user:{name:'PlaceHolder', handle:'placeholder', pic:pic}, time:new Date(), content:{text:'placeholder text on my placeholder tweet I am making a placeholder, lots of text lots of tweet not enough placeholder wwwwwwwwwwwwwwwwwwwwwwww wwwwwwwwwwwwwww wwwwwwwww wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww wwwwwwwwwwwwwwwwwwwww wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww', img:undefined, ref:false}, key:uniqid()},
+    {user:{name:'PlaceHolder', handle:'placeholder', pic:pic}, time:new Date(), content:{text:'placeholder text on my placeholder tweet I am making a placeholder, lots of text lots of tweet not enough placeholder wwwwwwwwwwwwwwwwwwwwwwww wwwwwwwwwwwwwww wwwwwwwww wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww wwwwwwwwwwwwwwwwwwwww wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww', img:undefined, ref:false}, key:uniqid()},
+    {user:{name:'PlaceHolder', handle:'placeholder', pic:pic}, time:new Date(), content:{text:'placeholder text on my placeholder tweet I am making a placeholder, lots of text lots of tweet not enough placeholder wwwwwwwwwwwwwwwwwwwwwwww wwwwwwwwwwwwwww wwwwwwwww wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww wwwwwwwwwwwwwwwwwwwww wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww', img:undefined, ref:false}, key:uniqid()},
+]
+
+const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
 class Home extends Component {
     render() {
@@ -25,7 +37,8 @@ const Main = (props) => {
     return (
         <div>
             <Topbar />
-            <Tweet />
+            <ComposeTweet />
+            <Feed tweets={fakeTweet}/>
         </div>
     )
 }
@@ -41,14 +54,14 @@ const Topbar = (props) => {
     )
 }
 
-const Tweet = (props) => {
+const ComposeTweet = (props) => {
     const autoSizeTextarea = (e) => {
         e.target.style.height = '50px'
         e.target.style.height = `${e.target.scrollHeight}px`;
     }
 
     return (
-        <div className={style.tweet}>
+        <div className={style.composeTweetContainer}>
             <div className={style.profileImg}>
                 <img alt='' src={pic}/>
             </div>
@@ -56,6 +69,58 @@ const Tweet = (props) => {
                 <textarea placeholder="What's Happening?" onKeyDown={autoSizeTextarea} maxLength='280'/>
                 <button>Tweet</button>
             </form>
+        </div>
+    )
+}
+
+const Feed = (props) => {
+    const {tweets} = props
+    return (
+        <div>
+            {tweets.map((post => {
+                return <Post tweet={post} key={post.key}/>
+            }))}
+        </div>
+    )
+}
+
+const Post = (props) => {
+    const {tweet} = props
+
+    const currentTime = new Date();
+    const postTime = tweet.time;
+    const timePassed = currentTime - postTime; //time in ms
+    let time = '';
+    if (timePassed > 86400000) { //days
+        time = `${months[postTime.getMonth()]} ${postTime.getDate()}`;
+    } else if (timePassed > 3600000) { //hours
+        time = `${Math.floor(timePassed / 3600000)}h`;
+    } else if (timePassed > 60000 ) { //mins
+        time = `${Math.floor(timePassed / 60000)}m`;
+    } else if (timePassed > 1000) {  //seconds
+        time = `${Math.floor(timePassed / 1000)}s`;
+    } else { //now
+        time = 'now';
+    }
+
+    return (
+        <div className={style.tweet}>
+            <div className={style.profileImg}>
+                <img alt='' src={tweet.user.pic}/>
+            </div>
+            <div className={style.userInfo}>
+                <h2>{tweet.user.name}</h2>
+                <h3>{tweet.user.handle}</h3>
+                <time>{time}</time>
+            </div>
+            <div className={style.content}>
+                <p>{tweet.content.text}</p>
+                {tweet.content.img && 
+                    <div className={style.postImg}>
+                        <img src={tweet.content.img} alt=''/>
+                    </div>
+                }
+            </div>
         </div>
     )
 }
