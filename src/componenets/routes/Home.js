e05dd14f39433f6945873796c4ccc09392370a69
Perry <petreli1@ymail.com>
2022-05-17 13:09:44 +0100

    added the compose tweet area - missing functionality

--- src/componenets/routes/Home.js
+++ src/componenets/routes/Home.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import Sidebar from '../Sidebar.js';
 import topStyle from '../styles/topbar.module.css';
+import style from '../styles/home.module.css';
+
+//temp image
+import pic from '../../images/logo.png';
 
 class Home extends Component {
     render() {
@@ -21,6 +25,7 @@ const Main = (props) => {
     return (
         <div>
             <Topbar />
+            <Tweet />
         </div>
     )
 }
@@ -35,5 +40,25 @@ const Topbar = (props) => {
         </div>
     )
 }
+
+const Tweet = (props) => {
+    const autoSizeTextarea = (e) => {
+        e.target.style.height = '50px'
+        e.target.style.height = `${e.target.scrollHeight}px`;
+    }
+
+    return (
+        <div className={style.tweet}>
+            <div className={style.profileImg}>
+                <img alt='' src={pic}/>
+            </div>
+            <form className={style.composeTweet}>
+                <textarea placeholder="What's Happening?" onKeyDown={autoSizeTextarea} maxLength='280'/>
+                <button>Tweet</button>
+            </form>
+        </div>
+    )
+}
+
 
 export default Home;
